d601b8faecabf5d20995532a3a52a8f775e45095
Christiand2020 <cmd6705@psu.edu>
2020-09-17 22:34:10 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,37 @@
+//Author:Christian Dell'Edera cmd6705@psu.edu
+//Collaborator:Stephanie Jen tzi5235@psu.edu
+//Collaborator:Corey Freas cxf5302@psu.edu
+//Collaborator:Jocelyn Fafard jgf5530@psu.edu
+//Section:11
+//Breakout:4
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+// You may assume n >= 0, return the sum of 0+1+2+3+...+ n
+int sum_n(int n){
+  if (n <= 0){
+    return 0;
+  } else{
+    return n + sum_n(n-1);
+  }
+}
+
+// You may assume n >= 0, print s n times, each time in its own line.
+void print_n(const char s, int n){
+  if (n <= 0){
+    return;
+  } else {
+    printf("%s\n", s);
+    print_n(s, n-1);
+  }
+}
+
+int main(void) {
+  charogNumber=readline("Enter an int: ");
+  int intNumber=atoi(ogNumber);
+  printf("sum is %d.\n", sum_n(intNumber));
+  char*Word=readline("Enter a string: ");
+  print_n(Word, intNumber);
+}
